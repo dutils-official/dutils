a10d6b71810c4a4a6a60037008cdd6c777a7a60a
RubyTheRoobster <71991501+RubyTheRoobster@users.noreply.github.com>
2021-08-06 13:29:26 -0400

    Delete package.d

--- package.d
+++
@@ -1,6 +0,0 @@
-/*dutils by Ruby The Roobster*/
-/*Version 0.2.5 Release(average of all the version numbers of the files)*/
-/*Some random modules. Use at free will*/
-module dutils;
-
-public import dutils.binom;
