3a98fa5814a743f7de5171bd75ef6742573ace55
RubyTheRoobster <71991501+RubyTheRoobster@users.noreply.github.com>
2021-08-25 13:06:23 -0400

    Adding license comment...

--- src/binom.d
+++ src/binom.d
@@ -2,6 +2,22 @@
 /* Version 0.2.5 Release*/
 /*Last Updated: 08/06/2021*/
 /*Module for handling binomials in the D Progrlamming Languge 2.0*/
+
+/*    This file is part of dutils.
+
+    dutils is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    dutils is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with dutils.  If not, see <https://www.gnu.org/licenses/>.*/
+
 module dutils.binom;
 
 public enum unknown { no = 0, yes = 1};
