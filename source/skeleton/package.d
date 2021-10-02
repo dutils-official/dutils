bafb88ebd2e2b881b72af87a1fc1368dfe73f8fd
RubyTheRoobster <michaeleverestc79@gmail.com>
2021-10-02 10:39:40 -0400

    h

--- source/skeleton/package.d
+++
@@ -1,7 +0,0 @@
-/** Copyright: 2021, Ruby The Roobster*/
-/**Author: Ruby The Roobster, michaeleverestc79@gmail.com*/
-/**Date: October 1, 2021*/
-/** License:  GPL-3.0**/
-
-module dutils.skeleton;
-public import dutils.skeleton.skeleton;
